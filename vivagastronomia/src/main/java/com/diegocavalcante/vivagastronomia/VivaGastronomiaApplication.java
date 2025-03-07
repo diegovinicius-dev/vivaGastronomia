99d2399260b3fbddd8c9c71461d220ff4f95e45c
Diego Vinicius <diegocavalcante.tech@gmail.com>
2025-03-06 23:15:37 -0300

    Initial commit: Projeto Spring Boot gerado pelo Spring Initializr

---
+++ vivagastronomia/src/main/java/com/diegocavalcante/vivagastronomia/VivaGastronomiaApplication.java
@@ -0,0 +1,13 @@
+package com.diegocavalcante.vivagastronomia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaGastronomiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivaGastronomiaApplication.class, args);
+	}
+
+}
---
+++ vivagastronomia/src/test/java/com/diegocavalcante/vivagastronomia/VivaGastronomiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegocavalcante.vivagastronomia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivaGastronomiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
